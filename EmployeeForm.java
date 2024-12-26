a048e97870eb0dbef7246efc0c40f3962bf85343
NtuthukoM <ntuthukorocks@gmail.com>
2024-12-26 21:58:10 +0200

    form ui with validation

--- EmployeeForm.java
+++ EmployeeForm.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class EmployeeForm {
 	
@@ -8,7 +9,19 @@ public class EmployeeForm {
 	private JLabel headerLabel;
 	private JLabel statusLabel;
 	private JPanel controlPanel;
-	private JLabel msglabel;
+	private JPanel formPanel;
+	//Form view:
+	JTextField tffName;
+	JTextField tfLname;
+	JTextField tfDesignation;
+    JComboBox listComboDepartment;
+	JComboBox listComboGender;
+	
+	JButton btnSave;
+	JButton btnCancel;
+	//Table view:
+	private JPanel tableViewPanel;
+	private JPanel navPanel;
 	
 	public EmployeeForm() {
 		prepareGUI();
@@ -16,7 +29,7 @@ public class EmployeeForm {
 	
 	public static void main(String[] args) {
 			EmployeeForm form = new EmployeeForm();
-			form.showCardLayoutDemo();
+			form.showForm();
 	}
 
 	private void prepareGUI() {
@@ -45,8 +58,123 @@ public class EmployeeForm {
 		panel.add(headerLabel);
 		panel.add(controlPanel);
 		panel.add(statusLabel);
-		mainFrame.getContentPane().add(panel, BorderLayout.CENTER);
+		
+		formPanel = new JPanel();
+		formPanel.setLayout(new GridLayout(6, 2, 20, 20));
+		mainFrame.getContentPane().add(formPanel, BorderLayout.CENTER);
 		mainFrame.setVisible(true);
+	}
+	
+	private void showForm() {
+		tffName = new JTextField(100);
+		tfLname = new JTextField(100);
+		tfDesignation = new JTextField(100);
+		
+		formPanel.add(new JLabel("First Name"));
+		formPanel.add(tffName);
+		formPanel.add(new JLabel("Last Name"));
+		formPanel.add(tfLname);
+		formPanel.add(new JLabel("Gender"));
+		final DefaultComboBoxModel panelName = new DefaultComboBoxModel();
+		panelName.addElement("");
+		panelName.addElement("Male");
+		panelName.addElement("Female");
+		listComboGender = new JComboBox(panelName);
+		listComboGender.setSelectedIndex(0);
+		JScrollPane listComboScrollPane = new JScrollPane(listComboGender);
+		
+		formPanel.add(listComboScrollPane);
+		formPanel.add(new JLabel("Designation"));
+		formPanel.add(tfDesignation);
+		formPanel.add(new JLabel("Department"));
+		
+		final DefaultComboBoxModel panelDepartment = new DefaultComboBoxModel();
+		panelDepartment.addElement("");
+		panelDepartment.addElement("Human Resources (HR)");
+		panelDepartment.addElement("Marketing and Sales");
+		panelDepartment.addElement("Finance");
+		panelDepartment.addElement("Operations/Production");
+		panelDepartment.addElement("Research and Development (R&D)");
+		panelDepartment.addElement("Customer Service");
+		panelDepartment.addElement("Information Technology (IT)");
+		panelDepartment.addElement("Administration/Legal");
+		listComboDepartment = new JComboBox(panelDepartment);
+		listComboDepartment.setSelectedIndex(0);
+		JScrollPane listComboScrollPaneDepartment = new JScrollPane(listComboDepartment);
+
+		formPanel.add(listComboScrollPaneDepartment);
+		formPanel.add(new JLabel(""));
+		
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		btnSave = new JButton("Save");
+		btnCancel = new JButton("Cancel");
+		buttons.add(btnSave);
+		buttons.add(btnCancel);
+		formPanel.add(buttons);
+		mainFrame.setVisible(true);
+		
+		//Listeners:
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String data = tffName.getText();
+				if(ValidateForm()) {
+					JOptionPane.showMessageDialog(formPanel, data, 
+                                          "INFORMATION", 
+                                          JOptionPane.INFORMATION_MESSAGE);	
+					ResetForm();
+				}
+
+			}
+		}); 
+		
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//open table, close and reset current form.
+				ResetForm();
+			}
+		}); 
+	}
+	
+	private void ResetForm() {
+		tffName.setText("");
+		tfLname.setText("");
+		tfDesignation.setText("");
+		listComboDepartment.setSelectedIndex(0);
+		listComboGender.setSelectedIndex(0);
+	}
+	
+	private Boolean ValidateForm() {
+		if(tffName.getText() == null || tffName.getText().trim().isEmpty()){
+            JOptionPane.showMessageDialog(formPanel, "Frist Name is required", 
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);			
+			return false;
+		}
+		if(tfLname.getText() == null || tfLname.getText().trim().isEmpty()){
+            JOptionPane.showMessageDialog(formPanel, "Last Name is required", 
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);			
+			return false;
+		}
+		
+		if( listComboGender.getSelectedIndex() == 0 || listComboGender.getSelectedIndex() == -1){
+            JOptionPane.showMessageDialog(formPanel, "Gender is required", 
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);			
+			return false;			
+		}
+		
+		if(tfDesignation.getText() == null || tfDesignation.getText().trim().isEmpty()){
+            JOptionPane.showMessageDialog(formPanel, "Designation is required", 
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);			
+			return false;
+		}
+		
+		if( listComboDepartment.getSelectedIndex() == 0 || listComboDepartment.getSelectedIndex() == -1){
+            JOptionPane.showMessageDialog(formPanel, "Department is required", 
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);			
+			return false;			
+		}
+		
+		return true;
 	}
 
 	private void showCardLayoutDemo() {
@@ -97,6 +225,29 @@ public class EmployeeForm {
 		controlPanel.add(showButton);
 		controlPanel.add(panel);
 		mainFrame.setVisible(true);
+	}
+	
+	class Employee
+	{
+		public String Fname;
+		public String Lname;
+		public String Gender;
+		public String Designation;
+		public String Department;
+	}
+	
+	class EmployeeManager {
+		
+		public ArrayList ReadEmployees() {
+			ArrayList<Employee> list = new ArrayList<Employee>();
+			//load employees:
+			
+			return list;
+		}
+		
+		public void SaveEmployee(Employee emp) {
+			//save employee:
+		}
 	}
 	
 }
